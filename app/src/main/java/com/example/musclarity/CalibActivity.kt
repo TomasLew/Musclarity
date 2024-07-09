af8765054fd90507b2252385fad0a03d7c02fdb7
Tomas Lew <lewtomas@gmail.com>
2024-07-09 14:15:32 -0300

    preparar para merge

--- app/src/main/java/com/example/musclarity/CalibActivity.kt
+++ app/src/main/java/com/example/musclarity/CalibActivity.kt
@@ -1,22 +1,19 @@
 package com.example.musclarity
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.Looper
-import android.os.Message
+import android.os.Handler
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Handler
-import android.content.SharedPreferences
-import android.widget.ImageView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.squareup.picasso.Picasso
 
 
 class CalibActivity : AppCompatActivity() {
@@ -28,6 +25,7 @@ class CalibActivity : AppCompatActivity() {
 
     var isCalibrating = false
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calibration_activity)
 
